84579d0b6174cf2e286687d28ebb27b0af92b928
viniciusteixeiradias <viniciustdias2001@gmail.com>
2022-10-26 21:07:29 -0300

    chore: stash code

--- src/components/AppCalculator.vue
+++ src/components/AppCalculator.vue
@@ -1,57 +1,205 @@
 <script setup lang="ts">
+import { reactive, ref, watch } from 'vue';
+import { StyleButton, Symbol } from '../models/StyleButton'
 import AppButton from './AppButton.vue'
-import AppLargeButton from './AppLargeButton.vue';
-import { StyleButton } from '../models/StyleButton'
-import { ref } from 'vue';
 
-const result = ref(new Intl.NumberFormat('br-BR').format(987654321));
+interface RowButtons {
+    type: StyleButton,
+    value: Symbol,
+    isLarge?: boolean
+}
+
+const result = ref('0');
+
+const validateValue = ():boolean => {
+  if (+result.value > 100) {
+    result.value = 'Error'
+  }
+  const maxLength = 11
+  return result.value.length < maxLength
+}
+
+const handleOthers = (value: Symbol) => {
+  switch (value) {
+    case Symbol.AC:
+      result.value = '0'
+      break
+    case Symbol.CLEAR:
+      result.value = '0'
+      break
+    case Symbol.EXPRESSION:
+      result.value.includes('-')
+        ? result.value = `${result.value.substring(1)}`
+        : result.value = `-${result.value}`
+      break
+    case Symbol.PERCENT:
+      result.value = (+result.value / 100).toString()
+  }
+}
+
+const handleOperation = (value: Symbol) => {
+  switch (value) {
+    case Symbol.AC || Symbol.CLEAR:
+      result.value = '0'
+      break
+  }
+}
+
+const setValue = (value: Symbol, type: StyleButton) => {
+  if (!validateValue) {
+    return
+  }
+
+  if (type === StyleButton.OPERATION) {
+    handleOperation(value)
+    return
+  }
+
+  if (type === StyleButton.OTEHR) {
+    handleOthers(value)
+    return
+  }
+
+  if (result.value === Symbol.ZERO) {
+    result.value = value
+  } else {
+    result.value = `${result.value}${value}`
+  }
+}
+
+const firstRowButtons = reactive<RowButtons[][]>([
+  [
+    {
+      type: StyleButton.OTEHR,
+      value: Symbol.AC
+    },
+    {
+      type: StyleButton.OTEHR,
+      value: Symbol.EXPRESSION
+    },
+    {
+      type: StyleButton.OTEHR,
+      value: Symbol.PERCENT
+    },
+    {
+      type: StyleButton.OPERATION,
+      value: Symbol.DIVISION
+    }
+  ],
+  [
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.SEVEN
+    },
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.EIGHT
+    },
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.NINE
+    },
+    {
+      type: StyleButton.OPERATION,
+      value: Symbol.MULTIPLICATION
+    }
+  ],
+  [
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.FOUR
+    },
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.FIVE
+    },
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.SIX
+    },
+    {
+      type: StyleButton.OPERATION,
+      value: Symbol.MINUS
+    }
+  ],
+  [
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.ONE
+    },
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.TWO
+    },
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.THREE
+    },
+    {
+      type: StyleButton.OPERATION,
+      value: Symbol.PLUS
+    }
+  ],
+  [
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.ZERO,
+      isLarge: true
+    },
+    {
+      type: StyleButton.DEFAULT,
+      value: Symbol.COMMA
+    },
+    {
+      type: StyleButton.OPERATION,
+      value: Symbol.EQUALS
+    }
+  ]
+])
+
+watch(() => result.value, (val) => {
+  if (!validateValue()) {
+    result.value = Number(val).toExponential()
+  }
+
+  if (!+val) {
+    firstRowButtons[0][0].value = Symbol.AC
+  } else if (+val && firstRowButtons[0][0].value !== Symbol.CLEAR) {
+    firstRowButtons[0][0].value = Symbol.CLEAR
+  }
+})
 </script>
 
 <template>
-    <span class="app-calculator__input">{{ result }}</span>
-    <div class="app-calculator">
-        <AppButton :style-button="StyleButton.OTEHR" symbol="AC" />
-        <AppButton :style-button="StyleButton.OTEHR" symbol="+/-" />
-        <AppButton :style-button="StyleButton.OTEHR" symbol="%" />
-        <AppButton :style-button="StyleButton.OPERATION" symbol="÷" />
-    </div>
-    <div class="app-calculator">
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="7" />
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="8" />
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="9" />
-        <AppButton :style-button="StyleButton.OPERATION" symbol="x" />
-    </div>
-    <div class="app-calculator">
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="4" />
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="5" />
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="6" />
-        <AppButton :style-button="StyleButton.OPERATION" symbol="-" />
-    </div>
-    <div class="app-calculator">
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="1" />
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="2" />
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="3" />
-        <AppButton :style-button="StyleButton.OPERATION" symbol="+" />
-    </div>
-    <div class="app-calculator">
-        <AppLargeButton symbol="0" />
-        <AppButton :style-button="StyleButton.DEFAULT" symbol="," />
-        <AppButton :style-button="StyleButton.OPERATION" symbol="=" />
-    </div>
+  <span class="app-calculator__input">{{ result }}</span>
+  <div 
+    class="app-calculator"
+    v-for="(_, index) in firstRowButtons" 
+    :key="index"
+  >
+    <AppButton 
+      v-for="(button, indexBtn) in firstRowButtons[index]" 
+      :key="indexBtn"
+      :is-large="button.isLarge"
+      :style-button="button.type"
+      :symbol="button.value"
+      @click="setValue(button.value, button.type)"
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .app-calculator {
-    display: flex;
-    gap: 0.75rem;
-    margin-block: 0.75rem;
+  display: flex;
+  gap: 0.75rem;
+  margin-block: 0.75rem;
 
-    &__input {
-        font-size: 3rem;
-        color: white;
-        display: flex;
-        justify-content: flex-end;
-        margin: 0 0.5rem 1.5rem 0;
-    }
+  &__input {
+    font-size: 3rem;
+    color: white;
+    display: flex;
+    justify-content: flex-end;
+    margin: 0 0.5rem 1.5rem 0;
+  }
 }
 </style>
